82d6dbe530cbd3310f29cbd99b24defeb8267098
canurdikfit <161978264+canurdikfit@users.noreply.github.com>
2024-07-17 20:17:11 +0100

    Add files via upload

--- src/components/IcePop.jsx
+++ src/components/IcePop.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Video from "./../assets/video.mp4";
+import TheBox from "./../assets/images/thebox.jpeg";
 
 export default function IcePop() {
   return (
@@ -8,7 +9,7 @@ export default function IcePop() {
       className="px-6 md:px-14 lg:px-24 py-20 flex items-center justify-center bg-white"
     >
       <div className="w-full overflow-hidden container bg-white rounded-3xl flex flex-col items-start justify-start border-4 border-black">
-        <video controls loop>
+        <video controls loop poster={TheBox}>
           <source src={Video} type="video/mp4" />
         </video>
       </div>
